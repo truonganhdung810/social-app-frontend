8735cb75b831d2db566ce92498ff967d961c1052
truonganhdung810 <truonganhdung810@gmail.com>
2025-05-21 13:26:47 +0700

    update cover

--- src/components/cover-image/CoverImage.jsx
+++ src/components/cover-image/CoverImage.jsx
@@ -1,14 +1,14 @@
-"use client";
-import { useState, useRef, useEffect } from "react";
-import "../cover-image/cover-image.css";
-import { FaCamera } from "react-icons/fa";
-import PopupMenu from "@/components/cover-image/PopupMenu";
+'use client'
+import { useState, useRef, useEffect } from 'react'
+import '../cover-image/cover-image.css'
+import { FaCamera } from 'react-icons/fa'
+import PopupMenu from '@/components/cover-image/PopupMenu'
 
-let imgUrl = "";
-let originImg = "https://images.unsplash.com/photo-1747372248943-33e9064aefab";
-let defaultOffsetY = 0;
-let defaultOffsetX = 0;
-let imageRatio = 1;
+let imgUrl = ''
+let originImg = 'https://images.unsplash.com/photo-1747372248943-33e9064aefab'
+let defaultOffsetY = 0
+let defaultOffsetX = 0
+let imageRatio = 1
 export default function CoverImage() {
   // Ảnh cover có 4 trường hợp kích thước:
   // 1200x400 đối với thiết bị trên 1280
@@ -22,119 +22,119 @@ export default function CoverImage() {
   // Nếu tỉ lệ ảnh là 3:1, không cần phải đặt vị trí
 
   // useState để lưu file Ảnh Cover được chọn từ máy
-  const [fileImage, setFileImage] = useState(null);
+  const [fileImage, setFileImage] = useState(null)
   // Url của Cover
-  const [coverUrl, setCoverUrl] = useState(originImg);
+  const [coverUrl, setCoverUrl] = useState(originImg)
 
-  const btnEditCoverRef = useRef(null);
-  const coverRef = useRef(null);
-  const coverContainerRef = useRef(null);
+  const btnEditCoverRef = useRef(null)
+  const coverRef = useRef(null)
+  const coverContainerRef = useRef(null)
 
   // Kích thước của Container chứa Cover
   // Được lấy ra ngay ban đầu khi kiểm tra kích thước màn hình thiết bị
-  const [fullWidth, setFullWidth] = useState();
-  const [fullHeight, setFullHeight] = useState();
+  const [fullWidth, setFullWidth] = useState()
+  const [fullHeight, setFullHeight] = useState()
 
   // State kiểm tra cover đã load được ảnh chưa
-  const [coverLoaded, setCoverLoaded] = useState(false);
+  const [coverLoaded, setCoverLoaded] = useState(false)
 
   // State bật tắt PopupMenu
-  const [isShowPopupMenu, setIsShowPopupMenu] = useState(false);
+  const [isShowPopupMenu, setIsShowPopupMenu] = useState(false)
 
   // State kiểm tra có đang ở trạng thái Preview Cover
-  const [isPreviewCover, setIsPreviewCover] = useState(false);
+  const [isPreviewCover, setIsPreviewCover] = useState(false)
 
   // Dùng useEffect kiểm tra ảnh cover tải xong, setState để hiển thị button
   useEffect(() => {
-    const img = coverRef.current;
+    const img = coverRef.current
 
     // Kiểm tra nếu ảnh đã load từ cache
     if (img && img.complete && img.naturalWidth !== 0) {
-      setCoverLoaded(true);
-      return;
+      setCoverLoaded(true)
+      return
     }
 
     // Gắn sự kiện onload và onerror
-    const handleLoad = () => setCoverLoaded(true);
-    const handleError = () => console.log("Không thể load ảnh");
+    const handleLoad = () => setCoverLoaded(true)
+    const handleError = () => console.log('Không thể load ảnh')
 
-    img.addEventListener("load", handleLoad);
-    img.addEventListener("error", handleError);
+    img.addEventListener('load', handleLoad)
+    img.addEventListener('error', handleError)
 
     // Dọn dẹp sự kiện khi component unmount
     return () => {
-      img.removeEventListener("load", handleLoad);
-      img.removeEventListener("error", handleError);
-    };
-  }, [coverRef]);
+      img.removeEventListener('load', handleLoad)
+      img.removeEventListener('error', handleError)
+    }
+  }, [coverRef])
 
   // Bật tắt PopupMenu bằng Button
   function clickBtnEditCover() {
-    setIsShowPopupMenu(!isShowPopupMenu);
+    setIsShowPopupMenu(!isShowPopupMenu)
   }
 
   // Các giá trị tọa độ để hiển thị cover
   // OffsetY là vị trí hiển thị của ảnh Cover so với container, đây là 1 trường cần lưu trữ kèm theo ảnh để hiển thị ảnh cho đẹp
-  const [offsetY, setOffsetY] = useState(defaultOffsetY);
-  const [offsetX, setOffsetX] = useState(defaultOffsetX);
-  const [isDragX, setIsDragX] = useState(false);
-  const [isDragY, setIsDragY] = useState(false);
-  const [startX, setStartX] = useState(null);
-  const [startY, setStartY] = useState(null);
-  const [dragging, setDragging] = useState(false);
-  const [containerHeight, setContainerHeight] = useState(400);
-  const [imageHeight, setImageHeight] = useState(800);
-  const [containerWidth, setContainerWidth] = useState(1200);
-  const [imageWidth, setImageWidth] = useState(1200);
+  const [offsetY, setOffsetY] = useState(defaultOffsetY)
+  const [offsetX, setOffsetX] = useState(defaultOffsetX)
+  const [isDragX, setIsDragX] = useState(false)
+  const [isDragY, setIsDragY] = useState(false)
+  const [startX, setStartX] = useState(null)
+  const [startY, setStartY] = useState(null)
+  const [dragging, setDragging] = useState(false)
+  const [containerHeight, setContainerHeight] = useState(400)
+  const [imageHeight, setImageHeight] = useState(800)
+  const [containerWidth, setContainerWidth] = useState(1200)
+  const [imageWidth, setImageWidth] = useState(1200)
 
   // Effect lấy ra chiều cao của image Cover khi kéo dọc, lấy ra width khi kéo ngang
   useEffect(() => {
-    const imgCom = new Image();
-    imgCom.src = coverUrl;
+    const imgCom = new Image()
+    imgCom.src = coverUrl
     imgCom.onload = () => {
-      console.log("Kich thuoc ảnh: " + imgCom.width + " x " + imgCom.height);
-      let w = imgCom.width;
-      let h = imgCom.height;
-      imageRatio = w / h;
-      console.log(imageRatio);
+      console.log('Kich thuoc ảnh: ' + imgCom.width + ' x ' + imgCom.height)
+      let w = imgCom.width
+      let h = imgCom.height
+      imageRatio = w / h
+      console.log(imageRatio)
       // Kiểm tra tỉ lệ của ảnh so với tỉ lệ 3:1 mặc định
       if (imageRatio > 3) {
         // di chuyển ngang
-        setIsDragX(true);
-        setIsDragY(false);
+        setIsDragX(true)
+        setIsDragY(false)
         // scale ảnh to lên hoặc nhỏ lại cho bằng height của container, width của ảnh to hơn width của container
-        const imgH = fullHeight;
-        const imgW = (w * fullHeight) / h;
-        console.log("Kich thuoc ảnh scale: " + imgW + " x " + imgH);
-        setImageHeight(imgH);
-        setImageWidth(imgW);
-        setOffsetY(0);
+        const imgH = fullHeight
+        const imgW = (w * fullHeight) / h
+        console.log('Kich thuoc ảnh scale: ' + imgW + ' x ' + imgH)
+        setImageHeight(imgH)
+        setImageWidth(imgW)
+        setOffsetY(0)
       } else if (imageRatio < 3) {
         // di chuyển dọc
         // scale ảnh to lên hoặc nhỏ lại cho bằng width của container, height của ảnh to hơn height của container
-        setIsDragY(true);
-        setIsDragX(false);
-        const imgH = (h * fullWidth) / w;
-        const imgW = fullWidth;
-        console.log("Kich thuoc ảnh scale: " + imgW + " x " + imgH);
-        setOffsetX(0);
-        setImageHeight(imgH);
-        setImageWidth(imgW);
+        setIsDragY(true)
+        setIsDragX(false)
+        const imgH = (h * fullWidth) / w
+        const imgW = fullWidth
+        console.log('Kich thuoc ảnh scale: ' + imgW + ' x ' + imgH)
+        setOffsetX(0)
+        setImageHeight(imgH)
+        setImageWidth(imgW)
       } else {
         // tỉ lệ bằng khoảng 3:1 (cho sai số 5%), ko cần drag ảnh
       }
-    };
-    imgCom.onerror = () => setError("Failed to load image");
-  }, [coverUrl]);
+    }
+    imgCom.onerror = () => setError('Failed to load image')
+  }, [coverUrl])
 
   // Lấy kích thước container-cover
   useEffect(() => {
     if (coverContainerRef.current) {
-      const w = coverContainerRef.current.offsetWidth; // Lấy chiều rộng container
-      const h = coverContainerRef.current.offsetHeight; // Lấy chiều dài container
-      console.log("container: ", w, h);
-      setFullWidth(w);
-      setFullHeight(h);
+      const w = coverContainerRef.current.offsetWidth // Lấy chiều rộng container
+      const h = coverContainerRef.current.offsetHeight // Lấy chiều dài container
+      console.log('container: ', w, h)
+      setFullWidth(w)
+      setFullHeight(h)
     }
 
     // Optional: Cập nhật width khi cửa sổ thay đổi kích thước
@@ -143,18 +143,18 @@ export default function CoverImage() {
         // Nếu có sự thay đổi width, lập tức hủy bỏ sự thay đổi Cover (đảm bảo hiển thị đẹp của Cover) -> gọi Hàm cancel
         // Người dùng muốn thay đổi cover, phải giữ nguyên width của thiết bị
         console.log(
-          "Cover Container Width",
+          'Cover Container Width',
           coverContainerRef.current.offsetWidth
-        );
-        setOffsetY(defaultOffsetY);
-        setOffsetX(defaultOffsetX);
-        cancelEdit();
+        )
+        setOffsetY(defaultOffsetY)
+        setOffsetX(defaultOffsetX)
+        cancelEdit()
       }
-    };
+    }
 
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
+    window.addEventListener('resize', handleResize)
+    return () => window.removeEventListener('resize', handleResize)
+  }, [])
 
   // Các hàm để thực hiện kéo thả vị trí cho ảnh Cover (dùng trên desktop, các hàm trên mobile phải set riêng)
   // Set 3 hàm này truyền vào cover-container để bắt sự kiện click, kéo, thả của chuột
@@ -164,55 +164,55 @@ export default function CoverImage() {
     if (isPreviewCover) {
       if (isDragY) {
         // Kéo thả theo chiều dọc (OffsetY)
-        setStartY(e.clientY);
-        console.log("start Y", startY);
-        setDragging(true);
+        setStartY(e.clientY)
+        console.log('start Y', startY)
+        setDragging(true)
       } else if (isDragX) {
         // Kéo thả theo chiều ngang (OffsetX)
-        setStartX(e.clientX);
-        console.log("start X", startX);
-        setDragging(true);
+        setStartX(e.clientX)
+        console.log('start X', startX)
+        setDragging(true)
       }
     }
-  };
+  }
 
   const handleMouseMove = (e) => {
-    if (!dragging) return;
+    if (!dragging) return
     if (isPreviewCover) {
       if (isDragY) {
         // Drag theo chiều dọc
-        const dy = e.clientY - startY;
-        setStartY(e.clientY);
+        const dy = e.clientY - startY
+        setStartY(e.clientY)
         // Giới hạn kéo không quá mức ảnh
         setOffsetY((prev) => {
-          const newOffset = prev + dy;
-          const maxOffset = 0;
-          const minOffset = containerHeight - imageHeight;
-          return Math.max(Math.min(newOffset, maxOffset), minOffset);
-        });
+          const newOffset = prev + dy
+          const maxOffset = 0
+          const minOffset = containerHeight - imageHeight
+          return Math.max(Math.min(newOffset, maxOffset), minOffset)
+        })
       } else if (isDragX) {
         // Drag theo chiều ngang
-        console.log("Client X", e.clientX);
-        const dx = e.clientX - startX;
-        setStartX(e.clientX);
+        console.log('Client X', e.clientX)
+        const dx = e.clientX - startX
+        setStartX(e.clientX)
         // Giới hạn kéo không quá mức ảnh
         setOffsetX((prev) => {
-          const newOffset = prev + dx;
-          const maxOffset = 0;
-          const minOffset = containerWidth - imageWidth;
-          return Math.max(Math.min(newOffset, maxOffset), minOffset);
-        });
+          const newOffset = prev + dx
+          const maxOffset = 0
+          const minOffset = containerWidth - imageWidth
+          return Math.max(Math.min(newOffset, maxOffset), minOffset)
+        })
       }
     }
-  };
+  }
 
   // Khi thả chuột
   const handleMouseUp = () => {
     if (isPreviewCover) {
-      console.log(offsetY);
-      setDragging(false);
+      console.log(offsetY)
+      setDragging(false)
     }
-  };
+  }
   // Kêt thúc các sự kiện cho việc kéo thả vị trí ảnh Cover
 
   const setNewCoverImage = (urlImG) => {
@@ -223,52 +223,51 @@ export default function CoverImage() {
     // Khởi tạo lại OffsetY=0 là giá trị mặc định, nếu có dữ liệu OffsetY theo ảnh thì cài đặt giá trị này
     // Ở đây chương trình không lưu lại giá trị OffsetY của ảnh nên dùng giá trị mặc định khi thay 1 ảnh cover mới
     // Nếu chạy trên mobile, giá trị OffsetY này phải là giá trị khác
-    setOffsetY(0);
-    setOffsetX(0);
-    setIsPreviewCover(true);
-    originImg = coverUrl;
-    setCoverUrl(urlImG);
-  };
+    setOffsetY(0)
+    setOffsetX(0)
+    setIsPreviewCover(true)
+    originImg = coverUrl
+    setCoverUrl(urlImG)
+  }
 
   function cancelEdit() {
-    console.log(originImg);
-    setCoverUrl(originImg);
-    setIsDragX(false);
-    setIsDragY(false);
-    setIsPreviewCover(false);
+    console.log(originImg)
+    setCoverUrl(originImg)
+    setIsDragX(false)
+    setIsDragY(false)
+    setIsPreviewCover(false)
   }
 
   // upload ảnh lên server
   async function uploadFile() {
-    const token = localStorage.getItem("token"); // Lấy token từ localStorage hoặc cookie
-    const userId = localStorage.getItem("id"); // id của người dùng từ localStorage
-    console.log("Token upload cover", token);
-    console.log("Id upload cover", userId);
-    const formData = new FormData();
-    formData.append("cover-image", fileImage);
+    const token = localStorage.getItem('token') // Lấy token từ localStorage hoặc cookie
+    const userId = localStorage.getItem('id') // id của người dùng từ localStorage
+    console.log('Token upload cover', token)
+    console.log('Id upload cover', userId)
+    const formData = new FormData()
+    formData.append('cover-image', fileImage)
 
-    const response = await fetch("http://localhost:4000/api/upload/", {
-      method: "POST",
+    const response = await fetch('http://localhost:4000/api/upload/cover', {
+      method: 'POST',
       headers: {
         authorization: `Bearer ${token}`, // Thêm token vào header Authorization
         id: userId,
         offsetX,
         offsetY,
-        "Content-Type": "multipart/form-data", // Nếu đang gửi file
       },
       body: formData, // Gửi FormData chứa ảnh
-    });
-    const res = response.json();
-    console.log("Response upload cover", res);
+    })
+    const res = response.json()
+    console.log('Response upload cover', res)
   }
 
   function saveEdit() {
-    originImg = coverUrl;
-    console.log(offsetY);
-    setIsDragX(false);
-    setIsDragY(false);
-    setIsPreviewCover(false);
-    uploadFile();
+    originImg = coverUrl
+    console.log(offsetY)
+    setIsDragX(false)
+    setIsDragY(false)
+    setIsPreviewCover(false)
+    uploadFile()
   }
   return (
     <div>
@@ -285,7 +284,7 @@ export default function CoverImage() {
         )}
         <div
           ref={coverContainerRef}
-          className={`cover-img-container ${dragging ? "dragging" : ""}`}
+          className={`cover-img-container ${dragging ? 'dragging' : ''}`}
           onMouseDown={handleMouseDown}
           onMouseMove={handleMouseMove}
           onMouseUp={handleMouseUp}
@@ -299,7 +298,7 @@ export default function CoverImage() {
               height: `${imageHeight}px`,
               width: `${imageWidth}px`,
               transform: ` translateX(${offsetX}px) translateY(${offsetY}px)`,
-              transition: dragging ? "none" : "transform 0.2s",
+              transition: dragging ? 'none' : 'transform 0.2s',
             }}
           ></img>
         </div>
@@ -328,5 +327,5 @@ export default function CoverImage() {
         )}
       </div>
     </div>
-  );
+  )
 }
